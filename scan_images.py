e85b03c1a77d4a0d43e48afbb38ce26b1e355b80
«Tehnik762» <yastroitel@gmail.com>
2024-01-31 12:45:44 +0100

    Fix multiprocessing scan_images New data, new categories

--- scan_images.py
+++ scan_images.py
@@ -2,15 +2,15 @@ from functions.image_analyser import processImage
 import os
 import pandas as pd
 import time
-from multiprocessing import Pool, freeze_support, set_start_method
+from multiprocessing import Pool
 # SETTINGS
 dir_to_scan = "images"
-exclude = [".DS_Store"]
+exclude = [".DS_Store", "0", "1"]
 # WORKFLOW
-start = time.time()
+
 dirs = os.scandir(dir_to_scan)
 
-def process_file(file_path):
+def process_file(file_path, start):
     data = []
     small_time = time.time()
     filename = os.path.basename(file_path)
@@ -26,7 +26,7 @@ def process_file(file_path):
     return data
 
 
-def process_folder(folder):
+def process_folder(folder, start):
     file_csv = (f"{folder.path}/{folder.name}.csv")
     if not os.path.exists(file_csv):
         db = pd.DataFrame()
@@ -40,13 +40,13 @@ def process_folder(folder):
         file_list = os.scandir(folder)
         for one_file in file_list:
             if one_file.name not in exclude and one_file.name not in already_scanned:
-                files_to_process.append(one_file.path)
+                files_to_process.append((one_file.path, start))
 
         #files_to_process = [file.path for file in os.scandir(folder_path)
         #                    if file.name not in already_scanned and file.name not in exclude]
 
         with Pool() as pool:
-            results = pool.map(process_file, files_to_process)
+            results = pool.starmap(process_file, files_to_process)
 
         data = [item for sublist in results for item in sublist]
 
@@ -58,9 +58,10 @@ def process_folder(folder):
 
 
 if __name__ == "__main__":
+    start = time.time()
     for directory in dirs:
         if directory.name not in exclude:
-            process_folder(directory)
+            process_folder(directory, start)
 
     elapsed_time = time.time() - start
     print(f"Final - {elapsed_time}")
