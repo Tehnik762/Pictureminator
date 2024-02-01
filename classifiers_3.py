c3f53186522a7cf7b4e528fd24f9b86f41c0fb01
Samuel Magda <sami.magda@gmail.com>
2024-02-01 18:15:00 +0100

    Added adaboost function

--- classifiers_3.py
+++ classifiers_3.py
@@ -17,27 +17,22 @@ def print_usage_lgbm():
     Usage of 3classifiers.py:
         from 3classifiers import lgbm_model
     Usage example:
-        best_model = lgbm_model(X_train, y_train, best_param)
-    
-    Ensure you have the following parameters ready before calling lgbm_model:
-    - X_train: Training features
-    - y_train: Training labels
-    - best_param: Dictionary of parameters for LightGBM
+        best_model = lgbm_model(X_train, y_train, param_grid)
 
    Predefined examples:          
-          best_param_lgbm = {
+          param_lgbm = {
                 'n_estimators': range(100, 400, 50), 
                 'num_leaves': range(20, 40, 5), 
                 'min_child_samples': range(1, 20, 2)}
     """)
 
-best_param_lgbm = {
+param_lgbm = {
     'n_estimators': range(100, 400, 50), 
     'num_leaves': range(20, 40, 5), 
     'min_child_samples': range(1, 20, 2)}
 
 
-def lgbm_model(X_train, y_train, best_param_lgbm):
+def lgbm_model(X_train, y_train, param_lgbm):
    
     # Creating a pipeline with LightGBM
     full_pipeline_lgbm = make_pipeline(
@@ -49,7 +44,7 @@ def lgbm_model(X_train, y_train, best_param_lgbm):
     # Parameter grid for LightGBM
     param_grid_lgbm = {
         f'lgbmclassifier__{key}': value if not isinstance(value, list) else value
-        for key, value in best_param_lgbm.items()
+        for key, value in param_lgbm.items()
     }
 
 
@@ -88,28 +83,23 @@ def print_usage_et():
     Usage of 3classifiers.py:
         from 3classifiers import extratrees_model
     Usage example:
-        best_model = extratrees_model(X_train, y_train, best_param)
-    
-    Ensure you have the following parameters ready before calling extratrees_model:
-    - X_train: Training features
-    - y_train: Training labels
-    - best_param: Dictionary of parameters for ExtraTreesClassifier
+        best_model = extratrees_model(X_train, y_train, param_grid)
 
     Predefined examples:                 
-        best_param_et = {
+        param_et = {
             'n_estimators': range(100, 400, 50), 
             'min_samples_split': range(2, 19, 2), 
             'min_samples_leaf': range(1, 13, 2)}
     """)
 
 
-best_param_et = {
+param_et = {
     'n_estimators': range(100,400,50),
     'min_samples_split': range(2,19,2),
     'min_samples_leaf': range(1,13,2),
 }
 
-def extratrees_model(X_train, y_train, best_param_et):
+def extratrees_model(X_train, y_train, param_et):
     # Creating a pipeline with ExtraTreesClassifier
     full_pipeline = make_pipeline(
         SimpleImputer(), 
@@ -120,7 +110,7 @@ def extratrees_model(X_train, y_train, best_param_et):
     # Parameter grid for ExtraTreesClassifier
     param_grid = {
         f'extratreesclassifier__{key}': value if not isinstance(value, list) else value
-        for key, value in best_param_et.items()
+        for key, value in param_et.items()
     }
 
     # GridSearchCV for ExtraTreesClassifier
@@ -156,29 +146,24 @@ def print_usage_cb():
     Usage of 3classifiers.py:
         from 3classifiers import catboost_model
     Usage example:
-        best_model = catboost_model(X_train, y_train, best_param)
-    
-    Ensure you have the following parameters ready before calling catboost_model:
-    - X_train: Training features
-    - y_train: Training labels
-    - best_param: Dictionary of parameters for CatBoostClassifier
+        best_model = catboost_model(X_train, y_train, param_grid)
 
     Predefined examples:                 
-        best_param_cb = {
+        param_cb = {
             'iterations': range(100, 400, 50), 
             'depth': range(4, 10, 2), 
             'l2_leaf_reg': np.logspace(-20, -19, 3)}
     """)
 
 
-best_param_cb ={
+param_cb ={
     'iterations': range(100, 400, 50),
     'depth': range(4, 10, 2),
     'l2_leaf_reg': np.logspace(-20, -19, 3), 
 }
 
 
-def catboost_model(X_train, y_train, best_param_cb):
+def catboost_model(X_train, y_train, param_cb):
     # Creating a pipeline with CatBoostClassifier
     full_pipeline_catboost = make_pipeline(
         SimpleImputer(), 
@@ -189,7 +174,7 @@ def catboost_model(X_train, y_train, best_param_cb):
     # Parameter grid for CatBoostClassifier
     param_grid_catboost = {
         f'catboostclassifier__{key}': value if not isinstance(value, list) else value
-        for key, value in best_param_cb.items()
+        for key, value in param_cb.items()
     }
 
     # GridSearchCV for CatBoostClassifier
@@ -215,4 +200,69 @@ def catboost_model(X_train, y_train, best_param_cb):
     print(f"Best score: {search_catboost.best_score_}")
 
     return search_catboost
+###################################################################################
+
+###################################################################################
+### AdaBoost Classifier
+###################################################################################
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+def print_usage_adaboost():
+    print("""
+    Usage of 3classifiers.py:
+        from 3classifiers import adaboost_model
+    Usage example:
+        best_model = adaboost_model(X_train, y_train, param_adaboost)
+
+   Predefined examples:          
+          param_adaboost = {
+                'n_estimators': range(50, 400, 50), 
+                'learning_rate': [0.01, 0.1, 1.0],
+                'base_estimator__max_depth': [1, 2, 3]}
+    """)
+
+param_adaboost = {
+    'n_estimators': range(50, 400, 50), 
+    'learning_rate': [0.01, 0.1, 1.0],
+    'base_estimator__max_depth': [1, 2, 3]
+}
+
+def adaboost_model(X_train, y_train, param_adaboost):
+    # Creating a pipeline with AdaBoost
+    full_pipeline_adaboost = make_pipeline(
+        SimpleImputer(), 
+        MinMaxScaler(), 
+        AdaBoostClassifier(base_estimator=DecisionTreeClassifier())
+    )
+
+    # Parameter grid for AdaBoost
+    param_grid_adaboost = {
+        f'adaboostclassifier__{key}': value if not isinstance(value, list) else value
+        for key, value in param_adaboost.items()
+    }
+
+    # GridSearchCV for AdaBoost
+    search_adaboost = GridSearchCV(
+        full_pipeline_adaboost,
+        param_grid_adaboost,
+        cv=10,
+        verbose=1,
+        n_jobs=multiprocessing.cpu_count() - 1
+    )
+    
+    # Timing the fit operation
+    start_time = time.time()
+    search_adaboost.fit(X_train, y_train)
+    end_time = time.time()
+    
+    total_time = end_time - start_time
+    minutes = total_time // 60
+    seconds = total_time % 60
+    print(f"Time taken to train the model: {int(minutes)} minutes and {seconds:.2f} seconds")
+
+    print(f"Best parameters: {search_adaboost.best_params_}")
+    print(f"Best score: {search_adaboost.best_score_}")
+
+    return search_adaboost
 ###################################################################################
