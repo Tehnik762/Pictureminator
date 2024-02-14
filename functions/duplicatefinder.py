6c18ebc81e3596510c74e55aede7b25df8467f51
Tehnik762 <yastroitel@gmail.com>
2024-02-14 16:13:50 +0100

    Fixed errors during scans

--- functions/duplicatefinder.py
+++ functions/duplicatefinder.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from pillow_heif import register_heif_opener
 import exifread
 from datetime import datetime
+import os
 
 
 def calculate_image_hash(image_path):
@@ -38,12 +39,12 @@ def group_similar_images(image_paths, image_data):
                     if cont and face1 == face2:
                         temp_dupl.append(path2)
                         processed_images.append(path2)
-        if len(temp_dupl) > 0:
-            temp_dupl.append(path1)
-            processed_images.append(path1)
-            duplicate_images.append(temp_dupl)
-        else:
-            if path1 not in processed_images:
+        if path1 not in processed_images:
+            if len(temp_dupl) > 0:
+                temp_dupl.append(path1)
+                processed_images.append(path1)
+                duplicate_images.append(temp_dupl)
+            else:
                 unique_images.append(path1)
 
     return unique_images, duplicate_images
@@ -65,4 +66,12 @@ def get_image_capture_time(image_path):
                 return capture_time
         except:
             pass
+    try:
+        filename = os.path.basename(image_path)
+        date_str = ".".join(filename.split(".")[:-1]).split("(")[0].rstrip()
+        capture_time = datetime.strptime(date_str, "%Y-%m-%d %H.%M.%S")
+        return capture_time
+    except Exception as e:
+        print("Error while extracting capture time from filename:", e)
+        pass
     return None
